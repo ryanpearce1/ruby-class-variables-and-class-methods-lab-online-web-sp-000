ce885166d25de1f0e60a1cefb00e2739f49883ea
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 20:47:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ attr_accessor :name, :artist, :genre
   
   def self.artist_count
     @@artists.each do |artist|
-      @@artist_count[artist] = @@artist_count(artist)
+      @@artist_count[artist] = @@artists.count(artist)
     end
     @@artist_count
   end
