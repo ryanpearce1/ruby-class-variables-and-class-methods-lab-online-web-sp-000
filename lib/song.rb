86f5a471aae2ba78b4b0092dbacb49f35191a5c8
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 20:47:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
+    @artists = artist
     @genre = genre
     @@count += 1
     @@genres << genre
